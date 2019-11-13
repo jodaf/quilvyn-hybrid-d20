90063b403dcc8abe941c2fb3ae9ce5c357f132a3
jodaf <spinaltap12345@gmail.com>
2019-11-12 17:44:53 -0800

    Update version.

--- HybridD20.js
+++ HybridD20.js
@@ -17,7 +17,7 @@ Place, Suite 330, Boston, MA 02111-1307 USA.
 
 "use strict";
 
-var HybridD20_VERSION = '0.1.0';
+var HybridD20_VERSION = '1.4.0.1alpha';
 
 /*
  * This module loads the rules for Hybrid D20.  The HybridD20 module
