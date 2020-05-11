04b344cea3ef5da0731de4d226a422b4f68bf8af
Jim Hayes <spinaltap12345@gmail.com>
2020-05-11 15:31:30 -0700

    Bump to v1.8.

--- HybridD20.js
+++ HybridD20.js
@@ -17,7 +17,7 @@ Place, Suite 330, Boston, MA 02111-1307 USA.
 
 "use strict";
 
-var HybridD20_VERSION = '1.6.0.1alpha';
+var HybridD20_VERSION = '1.8.0.1alpha';
 
 /*
  * This module loads the rules for Hybrid D20.  The HybridD20 module
